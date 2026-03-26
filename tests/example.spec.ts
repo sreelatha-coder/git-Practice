958ce6dca350a700073b01f3a2de695cf259fdc4
sreelatha-coder <srilatha9848654@gmail.com>
2026-03-26 03:11:28 -0500

    example spec file

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -7,6 +7,7 @@ test('has title', async ({ page }) => {
   await expect(page).toHaveTitle(/Playwright/);
 });
 
+
 test('get started link', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
